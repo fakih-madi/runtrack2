125acb35b37b20a70d601bf718804da35e7e2d1c
fakih <fakih.madi@laplateforme.io>
2020-11-04 14:34:02 +0100

    job 1 : impair et pair job2 : un caractère sur deux

---
+++ jour03/job01/index.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$nbr = [200, 204, 173, 98, 171, 404, 459];
+
+foreach ($nbr as $nbra) {
+  
+	if ($nbra%2 == 1)
+      echo "$nbra est impair <br/>";
+     else
+      echo "$nbra est pair <br/>";
+  
+}
+
+
+?>
