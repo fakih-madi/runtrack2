c16e17deae76f6d19fc9a81a0ec1416614be74ab
fakih <fakih.madi@laplateforme.io>
2020-11-08 11:47:22 +0100

    jour4 job1

---
+++ jour04/job01/index.php
@@ -0,0 +1,15 @@
+<form action="index.php" method="get">
+    Name: <input type="text" name="name"><br>
+    E-mail: <input type="text" name="email"><br>
+    First name: <input type="text" name="fname">
+    <input type="submit" name="submit">
+</form>
+
+<?php
+$nbrarg=0;
+
+foreach($_GET as $key => $value){
+    $nbrarg++;
+}
+echo $nbrarg;
+?>
